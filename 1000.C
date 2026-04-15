4bdf95520061598a4e6e9886b9e3c5cf8c5cb889
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-04-15 20:03:18 -0300

    Add header comments to 1000.C
    Added header comments with exercise details and objectives.

--- 1000.C
+++ 1000.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Gustavo Bernardes dos Santos
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : NNNN
+Data        : 15/04/2026
+Objetivo    : Mostrar "Hello world!"
+Aprendizado : Utilizar printf
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
