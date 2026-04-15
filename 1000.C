cae216bc5c591aade9e60f0d61be71d426b36ba0
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-04-15 20:06:54 -0300

    Update exercise number to 1000 in 1000.C

--- 1000.C
+++ 1000.C
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Gustavo Bernardes dos Santos
 Linguagem   : C
 Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : NNNN
+Exercício   : 1000
 Data        : 15/04/2026
 Objetivo    : Mostrar "Hello world!"
 Aprendizado : Utilizar printf
