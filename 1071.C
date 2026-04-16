60a4536d2fb0e0821dde63473f9282f01cdf3895
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-04-16 11:12:38 -0300

    Add program to sum odd numbers between two integers
    Implement logic to sum odd numbers between two input values.

---
+++ 1071.C
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Gustavo Bernardes dos Santos
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1071
+Data        : 16/04/2026
+Objetivo    : Soma de impares entre dois numeros
+Aprendizado : Uso de for 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+   int x, y, min, max, i, soma = 0;
+   
+   scanf("%d", &x);
+   scanf("%d", &y);
+   
+   if(x < y){
+       min = x;
+       max = y;
+   } 
+   else{
+       min = y;
+       max = x;
+   }
+  
+   for (i = min + 1; i < max; i++) {
+       if(i % 2 != 0){
+           soma = soma + i;
+       }
+   }
+   printf("%d\n", soma);
+   return 0;
+
+
+}
