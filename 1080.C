04f77074e587a811bf53f29f4c296d40a8dd54d1
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-04-23 23:06:14 -0300

    Update date and refactor main function logic

--- 1080.C
+++ 1080.C
@@ -4,29 +4,26 @@ Nome        : Gustavo Bernardes dos Santos
 Linguagem   : C
 Plataforma  : https://judge.beecrowd.com/pt/search
 Exercício   : https://judge.beecrowd.com/pt/problems/view/1080
-Data        : 15/04/2026
+Data        : 21/04/2026
 Objetivo    : 100 numeros mostrar o maior 
 Aprendizado : uso  de vetores
 -------------------------------------------------------------------------- */
 #include <stdio.h>
- 
-int main() {
-    
-    int x[100], maior = -1, posicao = -1, i;
-    
-    for (i = 0; i < 100; i++) {
+
+int main()
+{
+    int x[100], i, maior, posicao;
+    for(i=0; i<100; i++){
         scanf("%d", &x[i]);
-        
-        if (x[i] < 0) {
-            return 0;
-        }
-        
-        if (x[i] > maior) {
+    }
+    maior = x[0];
+    for(i=0; i<100; i++){
+       if(x[i] >  maior){
             maior = x[i];
             posicao = i + 1;
         }
     }
-    
-    printf("%d", maior);
-    printf("%d", posicao);
+    printf("%d\n", maior);
+    printf("%d\n", posicao);
+    return 0;
 }
