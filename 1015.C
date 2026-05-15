74656db9fdd9f85768e0916cc19c8bee49d8b0ba
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-05-15 10:14:46 -0300

    Refactor distance calculation into a separate function

--- 1015.C
+++ 1015.C
@@ -10,14 +10,17 @@ Aprendizado : utilizar funções da biblioteca math
 -------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <math.h>
- 
+
+float funcao(float a, float b, float c, float d){
+    return sqrt( pow(b - a, 2) + pow (d - c, 2));
+} 
 int main() {
  
     float x1, y1, x2, y2, distancia;
     scanf("%f %f", &x1, &y1);
     scanf("%f %f", &x2, &y2);
     
-    distancia = sqrt( pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    distancia = funcao(x1, x2, y1, y2);
     printf("%.4f\n", distancia);
     
  
