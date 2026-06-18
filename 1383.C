a575c7c67d3876d35fae7e04225722a1831b944f
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-06-18 13:52:35 -0300

    Refactor Sudoku validation logic and formatting

--- 1383.C
+++ 1383.C
@@ -9,14 +9,13 @@ Objetivo    : verificar se uma matriz 9×9 preenchida representa uma solução v
 Aprendizado : manipulação de matrizes
 -------------------------------------------------------------------------- */
 #include <stdio.h>
-
 int x[9][9];
 
-int valido(int a[9]) {
+int valido(int a[9]){
     int visto[10] = {0};
-    for (int i = 0; i < 9; i++) {
+    for(int i = 0; i < 9; i++){
         int v = a[i];
-        if (v < 1 || v > 9 || visto[v]) {
+        if(v < 1 || v > 9 || visto[v]){
             return 0;
         }
         visto[v] = 1;
@@ -24,46 +23,44 @@ int valido(int a[9]) {
     return 1;
 }
 
-int main() {
+int main(){
     int n;
     if (scanf("%d", &n) != 1) return 0;
 
-    for (int f = 0; f < n; f++) {
+    for(int f = 0; f < n; f++){
         int ok = 1;
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
                 scanf("%d", &x[i][j]);
             }
         }
-
-        for (int i = 0; i < 9 && ok; i++) {
-            if (!valido(x[i])) {
-                ok = 0;
+    
+        for(int i = 0; i < 9; i++){
+            int linha[9];
+            for(int j = 0; j < 9; j++){
+                linha[j] = x[i][j];
             }
+            if (!valido(linha)) ok = 0;
         }
 
-        for (int j = 0; j < 9 && ok; j++) {
+        for(int j = 0; j < 9; j++){
             int coluna[9];
-            for (int i = 0; i < 9; i++) {
+            for(int i = 0; i < 9; i++){
                 coluna[i] = x[i][j];
             }
-            if (!valido(coluna)) {
-                ok = 0;
-            }
-        }     
+            if (!valido(coluna)) ok = 0;
+        }
 
-        for (int i = 0; i < 9 && ok; i += 3) {
-            for (int j = 0; j < 9 && ok; j += 3) {
+        for(int bi = 0; bi < 9; bi += 3){
+            for(int bj = 0; bj < 9; bj += 3){
                 int bloco[9], r = 0;
-                for (int k = 0; k < 3; k++) {
-                    for (int l = 0; l < 3; l++) {
-                        bloco[r++] = x[i + k][j + l];
+                for(int l = 0; l < 3; l++){
+                    for(int k = 0; k < 3; k++){
+                        bloco[r++] = x[l + bi][k + bj];
                     }
                 }
-                if (!valido(bloco)) {
-                    ok = 0;
-                }
+                if (!valido(bloco)) ok = 0;
             }
         }
 
@@ -74,7 +71,6 @@ int main() {
             printf("NAO\n");
         }
         printf("\n");
-    }
-
-    return 0;
+    }   
+    return 0; 
 }
